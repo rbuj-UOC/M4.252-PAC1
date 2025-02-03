5419b497bc4031b8a5f9ef00f4470588cb6b0bd7
Robert Buj <robert.buj@gmail.com>
2025-02-03 13:06:36 +0100

    format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import html from "eslint-plugin-html";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import html from 'eslint-plugin-html';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.html"], plugins: { html } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  eslintConfigPrettier,
-];
+export default [{ files: ['**/*.html'], plugins: { html } }, { languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended, eslintConfigPrettier];
